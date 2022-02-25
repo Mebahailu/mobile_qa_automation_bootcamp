a90f07c59fd997ee5496e10222d7ba39c4bac2b8
Mebeahailu <mebea@qtf.io>
2022-02-25 19:13:54 +0800

    write test 02

---
+++ meba_test01.py
@@ -0,0 +1,58 @@
+import pytest
+import inspect
+import logging
+from appium.webdriver.webdriver import WebDriver
+
+
+# basic logger configrations
+# log level is info
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s: %(levelname)s : %(message)s')
+
+
+# just creating webdriver and initializing driver methods
+class WebCommon(WebDriver):
+    def __init__(self):
+        super().__init__()
+
+
+class Test01Android():
+    caller_name = ''
+    main_os = ''
+
+    def current_called_process(self):
+        all_stack_frames = inspect.stack()
+        caller_stack_frame = all_stack_frames[1]
+        Test01Android.caller_name = caller_stack_frame[3]
+
+    @pytest.mark.parametrize('os', [('android')])
+    def test_01(self, os):
+        Test01Android.main_os = os
+        #assert self.current_called_process() == 'Andriod'
+        #logging.info(f'{self.__class__.__name__}, {self.caller_name}')
+        assert self.main_os == 'android'
+        logging.info(
+            f'{Test01Android.caller_name}, OS = {Test01Android.main_os}')
+
+    @classmethod
+    def setup_class(cls):
+        tag = Test01Android()
+        tag.current_called_process()
+        logging.info(f'{cls.caller_name}')
+
+    @classmethod
+    def teardown_class(cls):
+        tag = Test01Android()
+        tag.current_called_process()
+        logging.info(f'{cls.caller_name}')
+
+    def setup_method(self):
+        self.current_called_process()
+        logging.info(f'{Test01Android.caller_name}')
+
+    def teardown_method(self):
+        self.current_called_process()
+        logging.info(f'{Test01Android.caller_name}')
+
+
