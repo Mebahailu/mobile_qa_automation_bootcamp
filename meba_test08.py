b6392b83d60a1adb23eaf6c6537906f2949013ae
Mebahailu <99938691+Mebahailu@users.noreply.github.com>
2022-03-14 15:42:31 +0800

    Update meba_test08.py

--- meba_test08.py
+++ meba_test08.py
@@ -11,4 +11,15 @@ desired_capa = {
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub/',disired_capa)
 
 
-driver.find_element(By.XPATH, '').click()
+def test_08_scroll(driver):
+    self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'List Demo').click()
+    self.driver.implicitly_wait(1)
+    actions = ActionChains(self.driver)
+    actions.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+    actions.w3c_actions.pointer_action.move_to_location(570, 1344)
+    actions.w3c_actions.pointer_action.pointer_down()
+    actions.w3c_actions.pointer_action.move_to_location(579, 512)
+    actions.w3c_actions.pointer_action.release()
+    actions.perform()
+    log.info("Check last item is available")
+    return self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Stratus')
